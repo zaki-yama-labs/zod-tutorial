ec27504bfd963725118931c17bc8006bc4f015d4
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2022-10-31 00:20:27 +0900

    03. Create an Array of Custom Types

--- src/03-array.problem.ts
+++ src/03-array.problem.ts
@@ -7,12 +7,13 @@ const StarWarsPerson = z.object({
   name: z.string(),
 });
 
-const StarWarsPeopleResults = z.unknown();
-//                            ^ 🕵️‍♂️
+const StarWarsPeopleResults = z.object({
+  results: z.array(StarWarsPerson),
+});
 
 export const fetchStarWarsPeople = async () => {
   const data = await fetch("https://swapi.dev/api/people/").then((res) =>
-    res.json(),
+    res.json()
   );
 
   const parsedData = StarWarsPeopleResults.parse(data);

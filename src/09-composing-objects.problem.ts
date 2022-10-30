6463c245e47efbf6a235f777ddc85a86baeca7d9
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2022-10-31 00:51:52 +0900

    09. Reduce Duplicated Code by Composing Schemas

--- src/09-composing-objects.problem.ts
+++ src/09-composing-objects.problem.ts
@@ -5,20 +5,21 @@ import { Equal, Expect } from "./helpers/type-utils";
  * 🕵️‍♂️ Refactor this code below to reduce the duplication,
  * while also making sure the cases don't go red!
  */
+const UUID = z.string().uuid();
 
 const User = z.object({
-  id: z.string().uuid(),
+  id: UUID,
   name: z.string(),
 });
 
 const Post = z.object({
-  id: z.string().uuid(),
+  id: UUID,
   title: z.string(),
   body: z.string(),
 });
 
 const Comment = z.object({
-  id: z.string().uuid(),
+  id: UUID,
   text: z.string(),
 });
 
@@ -27,5 +28,5 @@ type cases = [
   Expect<
     Equal<z.infer<typeof Post>, { id: string; title: string; body: string }>
   >,
-  Expect<Equal<z.infer<typeof User>, { id: string; name: string }>>,
+  Expect<Equal<z.infer<typeof User>, { id: string; name: string }>>
 ];

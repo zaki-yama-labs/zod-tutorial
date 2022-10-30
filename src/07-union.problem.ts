0ff89eb72cbe13d8fd43ee681dbfa2d9b7fd85cd
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2022-10-31 00:41:58 +0900

    07. Be Specific with Allowed Types

--- src/07-union.problem.ts
+++ src/07-union.problem.ts
@@ -5,8 +5,7 @@ import { z } from "zod";
 
 const Form = z.object({
   repoName: z.string(),
-  privacyLevel: z.string(),
-  //              ^ 🕵️‍♂️
+  privacyLevel: z.union([z.literal("private"), z.literal("public")]),
 });
 
 export const validateFormInput = (values: unknown) => {
@@ -22,7 +21,7 @@ it("Should fail if an invalid privacyLevel passed", async () => {
     validateFormInput({
       repoName: "mattpocock",
       privacyLevel: "something-not-allowed",
-    }),
+    })
   ).toThrowError();
 });
 
@@ -31,13 +30,13 @@ it("Should permit valid privacy levels", async () => {
     validateFormInput({
       repoName: "mattpocock",
       privacyLevel: "private",
-    }).privacyLevel,
+    }).privacyLevel
   ).toEqual("private");
 
   expect(
     validateFormInput({
       repoName: "mattpocock",
       privacyLevel: "public",
-    }).privacyLevel,
+    }).privacyLevel
   ).toEqual("public");
 });
